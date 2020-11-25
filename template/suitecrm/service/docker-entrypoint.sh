df163e542823eb49ac6cb47249e92ee384dc67e9
david.ngo <david.ngo@compareasiagroup.com>
2020-11-25 16:54:34 +0800

    Fixing the entrypoint

--- template/suitecrm/service/docker-entrypoint.sh
+++ template/suitecrm/service/docker-entrypoint.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
-set -euo pipefail
+#!/bin/sh
+set -e
 
 WEB_DIR="/var/www/html"
 echo "!!! Web Root Directory !!!"
 echo ${WEB_DIR}
 
-cd ${WEB_DIR}
+cd "${WEB_DIR}"
+
+chmod g+s /var/www/html/cache
+chmod 775 /tmp
 
 ./vendor/bin/robo repair:rebuild-extensions
 
-apache2-foreground
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- apache2-foreground "$@"
+fi
+
+exec "$@"
